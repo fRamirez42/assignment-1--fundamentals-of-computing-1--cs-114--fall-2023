fee69732f96254c67ebda7f1cd324de424abb8b3
Foxyz24 <feliperamirez2004@gmail.com>
2023-09-18 15:36:38 -0400

    Added the FR blocks and Farenheit to celcius

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,50 @@
 // Import libraries here
+import java.util.Scanner;
 
 public class Assignment1 {
   public static void main(String[] args) {
-    // Add your code here
+    
+    //Block names to be put here
+
+    System.out.println("FFFFFFFFF   RRRRRRRRRR");
+    System.out.println("FF          RR       RR");
+    System.out.println("FF          RR       RR");
+    System.out.println("FFFFFF      RR      RR");
+    System.out.println("FF          RRRRRRRR");
+    System.out.println("FF          RR     RR");
+    System.out.println("FF          RR      RR");
+    System.out.println("FF          RR       RR");
+    System.out.println();
+    
+
+
+
+
+
+
+    //Farenheit to Celcius
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("enter a number in Fahrenheit: ");
+
+    double number = scan.nextDouble();
+
+    double celciusp1 = (number - 32);
+
+    double celcius = celciusp1 * 5/9;
+
+    //5 characters string
+
+    String javaString;
+
+    System.out.println("Enter a 5 character word: ");
+
+    javaString = scan.nextLine();
+
+    int stringSize = javaString.length();
+
+    System.out.println(stringSize);
+
   }
 }
