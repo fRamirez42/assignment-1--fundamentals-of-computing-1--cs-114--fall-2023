60ea2c1b34016c0a60e0f2849651ae5cf22d4ba4
Foxyz24 <feliperamirez2004@gmail.com>
2023-09-18 16:01:59 -0400

    Added Random Number creation

--- Assignment1.java
+++ Assignment1.java
@@ -1,4 +1,5 @@
 // Import libraries here
+import java.util.Random;
 import java.util.Scanner;
 
 public class Assignment1 {
@@ -15,36 +16,56 @@ public class Assignment1 {
     System.out.println("FF          RR      RR");
     System.out.println("FF          RR       RR");
     System.out.println();
-    
-
-
-
-
 
 
     //Farenheit to Celcius
+
+    double number = 0.0;
 
     Scanner scan = new Scanner(System.in);
 
     System.out.println("enter a number in Fahrenheit: ");
 
-    double number = scan.nextDouble();
+    number = scan.nextDouble();
 
     double celciusp1 = (number - 32);
 
     double celcius = celciusp1 * 5/9;
 
+    System.out.println(celcius + " C");
+
+
     //5 characters string
 
-    String javaString;
+    String String5;
+
+    int stringSize = 0;
+
+    //while(stringSize != 5){ 
 
     System.out.println("Enter a 5 character word: ");
 
-    javaString = scan.nextLine();
+    String5 = scan.nextLine();
 
-    int stringSize = javaString.length();
+    stringSize = String5.length();
+
+    //}
 
     System.out.println(stringSize);
+    
+
+    //Create a random number
+
+    Random r = new Random();
+
+    int low = 32;
+
+    int high = 16384;
+
+    int result = r.nextInt(high-low) + low;
+
+    System.out.println(result);
+
 
   }
 }
